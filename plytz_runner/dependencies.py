a14886ae87eefca79509813a70c8b764b71dbd0d
Eric Magalhães <eric@magalhaes.com.br>
2023-05-30 20:27:44 +0200

    removing unecessary stuff

--- plytz_runner/dependencies.py
+++ plytz_runner/dependencies.py
@@ -1,11 +1,4 @@
 import os
 
-from typing import Annotated
-from fastapi import Header, HTTPException
-
 WORKDIR = os.environ["WORKDIR"]
-
-async def check_auth_token(x_plytz_auth: Annotated[str, Header()]):
-    if x_plytz_auth != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
